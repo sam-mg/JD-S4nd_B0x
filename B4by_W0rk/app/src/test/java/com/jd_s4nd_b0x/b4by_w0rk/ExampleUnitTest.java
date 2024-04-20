221e9aeddf4e11a428ce8581b31c1d71f80a8e7d
sam-mg <147686318+sam-mg@users.noreply.github.com>
2024-04-20 18:23:45 +0530

    Create B4by_W0rk

---
+++ B4by_W0rk/app/src/test/java/com/jd_s4nd_b0x/b4by_w0rk/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.jd_s4nd_b0x.b4by_w0rk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
