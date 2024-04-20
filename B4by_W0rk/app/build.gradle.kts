7c631b94f4cbd8b9b166278e9a040a6f44559d24
sam-mg <147686318+sam-mg@users.noreply.github.com>
2024-04-20 21:13:26 +0530

    Added Dependencies to the App

--- B4by_W0rk/app/build.gradle.kts
+++ B4by_W0rk/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -32,7 +33,37 @@ android {
 }
 
 dependencies {
+//    Dependencies For Retrofit
+//    This dependency facilitates sending and receiving GET and POST requests from APIs over the internet.
+//    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation(libs.retrofit)
+//    This dependency is the Gson Converter, developed by Google to convert JSON responses into Java objects.
+//    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation(libs.converter.gson)
+//    This is the Logging Interceptor dependency, used to log all HTTP requests sent and received.
+//    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.logging.interceptor)
 
+//    Dependencies for Room Database
+//    Dependency for working with SQL DataBase
+    implementation(libs.room.runtime)
+//    Dependency for work with database-related code during compilation
+    annotationProcessor(libs.room.compiler)
+
+//    A Powerful library for Loading and Displaying images
+//    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation(libs.picasso)
+
+//    Dependency for Accessing Resources
+//    This is a dependency for AndroidX Core library providing utility classes for resource management and permission handling.
+//    implementation("androidx.core:core:1.13.0")
+    implementation(libs.core)
+
+//    Dependency for FireBase Authentication
+//    This is a dependency used for authenticating individuals; new users can register, while already registered users can log in.
+    implementation(libs.firebase.auth)
+
+//    Default Implementations
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
