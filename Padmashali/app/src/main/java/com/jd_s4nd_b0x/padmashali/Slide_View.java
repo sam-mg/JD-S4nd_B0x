4f54b32f9126d009c52cf8c01776ca6d427774bb
sam-mg <147686318+sam-mg@users.noreply.github.com>
2024-05-15 17:19:02 +0530

    Added Partial Data

--- Padmashali/app/src/main/java/com/jd_s4nd_b0x/padmashali/Slide_View.java
+++ Padmashali/app/src/main/java/com/jd_s4nd_b0x/padmashali/Slide_View.java
@@ -22,7 +22,7 @@ public class Slide_View extends AppCompatActivity {
     private ImageButton btnPrevious, btnNext;
     private ImageView yearImage;
 
-    private int[] years = {2010, 2015, 2020};
+    private int[] years = {1804, 1810, 1818, 1820, 1830, 1842, 1844, 1852, 1855, 1857, 1960, 1970, 1980, 1990, 2000, 2010, 2020};
     private int currentYearIndex = 0;
 
     @SuppressLint("WrongViewCast")
@@ -92,10 +92,38 @@ public class Slide_View extends AppCompatActivity {
     private int getYearImage(int year) {
         // Replace with your logic to get image resource for each year
         switch (year) {
+            case 1804:
+                return R.drawable.ic_image_1804;
+            case 1810:
+                return R.drawable.ic_image_1810;
+            case 1818:
+                return R.drawable.ic_image_1818;
+            case 1820:
+                return R.drawable.ic_image_1820;
+            case 1830:
+                return R.drawable.ic_image_1830;
+            case 1842:
+                return R.drawable.ic_image_1842;
+            case 1844:
+                return R.drawable.ic_image_1844;
+            case 1852:
+                return R.drawable.ic_image_1852;
+            case 1855:
+                return R.drawable.ic_image_1855;
+            case 1857:
+                return R.drawable.ic_image_1857;
+            case 1960:
+                return R.drawable.ic_image_1960;
+            case 1970:
+                return R.drawable.ic_image_1970;
+            case 1980:
+                return R.drawable.ic_image_1980;
+            case 1990:
+                return R.drawable.ic_image_1990;
+            case 2000:
+                return R.drawable.ic_image_2000;
             case 2010:
                 return R.drawable.ic_image_2010;
-            case 2015:
-                return R.drawable.ic_image_2015;
             case 2020:
                 return R.drawable.ic_image_2020;
             default:
