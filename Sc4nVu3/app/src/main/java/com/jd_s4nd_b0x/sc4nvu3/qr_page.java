ab93856ac71dc044fa648b7abf2e47c4539e10e7
sam-mg <147686318+sam-mg@users.noreply.github.com>
2024-06-17 03:34:18 +0530

    Completed Front Page

--- Sc4nVu3/app/src/main/java/com/jd_s4nd_b0x/sc4nvu3/qr_page.java
+++ Sc4nVu3/app/src/main/java/com/jd_s4nd_b0x/sc4nvu3/qr_page.java
@@ -1,8 +1,6 @@
 package com.jd_s4nd_b0x.sc4nvu3;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button button;
+public class qr_page extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_qr_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        button = findViewById(R.id.welcome_btn);
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, qr_page.class);
-            startActivity(intent);
         });
     }
 }
