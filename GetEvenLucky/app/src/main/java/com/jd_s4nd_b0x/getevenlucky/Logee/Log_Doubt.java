891ae2e67c4449fee25e68f160150484a237d7da
sam-mg <147686318+sam-mg@users.noreply.github.com>
2024-09-02 15:29:27 +0530

    Make MainActivity & Logee

--- GetEvenLucky/app/src/main/java/com/jd_s4nd_b0x/getevenlucky/Logee/Log_Doubt.java
+++ GetEvenLucky/app/src/main/java/com/jd_s4nd_b0x/getevenlucky/Logee/Log_Doubt.java
@@ -1,6 +1,10 @@
 package com.jd_s4nd_b0x.getevenlucky.Logee;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.jd_s4nd_b0x.getevenlucky.Happy;
 import com.jd_s4nd_b0x.getevenlucky.R;
 
 public class Log_Doubt extends AppCompatActivity {
-
+    private String encryptedFlag;
+    private EditText editTextFlag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,17 @@ public class Log_Doubt extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        encryptedFlag = a.getEncryptedFlag();
+        Log.d("MainActivity", "Flag: " + encryptedFlag);
+        editTextFlag = findViewById(R.id.editTextFlag);
+        Button buttonSubmit = findViewById(R.id.buttonSubmit);
+        buttonSubmit.setOnClickListener(v -> {
+            String enteredFlag = editTextFlag.getText().toString().trim();
+            if (encryptedFlag.equals(enteredFlag)) {
+                Intent intent = new Intent(Log_Doubt.this, Happy.class);
+                startActivity(intent);
+            }
         });
     }
 }
