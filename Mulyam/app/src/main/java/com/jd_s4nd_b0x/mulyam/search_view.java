6ded6e398888b2262947939e9a6ab423d117dc01
sam-mg <147686318+sam-mg@users.noreply.github.com>
2024-10-11 23:53:53 +0530

    Complete Search View

--- Mulyam/app/src/main/java/com/jd_s4nd_b0x/mulyam/search_view.java
+++ Mulyam/app/src/main/java/com/jd_s4nd_b0x/mulyam/search_view.java
@@ -1,12 +1,17 @@
 package com.jd_s4nd_b0x.mulyam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class search_view extends AppCompatActivity {
 
@@ -19,6 +24,19 @@ public class search_view extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TextInputEditText textInputEditText = findViewById(R.id.stocknameInput);
+        Button searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(v -> {
+            String stockname = textInputEditText.getText() != null ? textInputEditText.getText().toString() : "";
+            if (stockname.isEmpty()) {
+                Toast.makeText(this, "Enter some Stock Name", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Getting Stock Details", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, loading_view.class);
+                intent.putExtra("stockName", stockname);
+                startActivity(intent);
+            }
         });
     }
 }
